2da92abb8f19d2f6521718ff011e96ddc162197a
Danny Cohen <danny@dannycohen.design>
2020-08-06 23:07:36 +0000

    Prettier

--- page.js
+++ page.js
@@ -1,15 +1,14 @@
-(function($) {
-	
-// Show our site navigation bar
-	$("#toggle-mobile-nav").click(function( event ) {
-	    event.preventDefault();
-	    $("body").toggleClass('navigationShown');
-	});
-		
-	$(document).keyup(function(e) {
-    	if (e.key === "Escape" && $("body").hasClass('navigationShown') ) { // escape key maps to keycode `27`
-        	 $("body").toggleClass('navigationShown');
-    	}
-	});
+(function ($) {
+  // Show our site navigation bar
+  $("#toggle-mobile-nav").click(function (event) {
+    event.preventDefault();
+    $("body").toggleClass("navigationShown");
+  });
 
-})( jQuery );
+  $(document).keyup(function (e) {
+    if (e.key === "Escape" && $("body").hasClass("navigationShown")) {
+      // escape key maps to keycode `27`
+      $("body").toggleClass("navigationShown");
+    }
+  });
+})(jQuery);
